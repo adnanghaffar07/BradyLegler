8bc11faa25eb0b8b5ccca49c9e7b3b71c8a07226
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2026-03-13 02:18:18 +0500

    Update index.tsx

--- templates/ArtworkTemplate/ArtworkEnquiryForm/index.tsx
+++ templates/ArtworkTemplate/ArtworkEnquiryForm/index.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React, { useState, useCallback } from 'react';
-import FormSidePanel from '@/components/Form/FormSidePanel';
-import Field from '@/components/Field';
+import React, { useCallback } from 'react';
 import Button from '@/components/Button';
+import { useContactSidebar } from '@/tools/hooks/useContactSidebar';
 
 type ArtworkEnquiryFormProps = {
   isOnSale: boolean;
@@ -11,24 +10,17 @@ type ArtworkEnquiryFormProps = {
 
 const ArtworkEnquiryForm: React.FC<ArtworkEnquiryFormProps> = props => {
   const { isOnSale } = props;
-  const [showForm, setShowForm] = useState(false);
+  const { openContactSidebar } = useContactSidebar();
 
   const handleClick = useCallback(() => {
     if (isOnSale) {
-      setShowForm(true);
+      openContactSidebar();
     }
-  }, [isOnSale]);
+  }, [isOnSale, openContactSidebar]);
 
   return (
     <>
       <Button onClick={handleClick} text={isOnSale ? 'Inquire' : 'Sold'} disabled={!isOnSale} variant="square" />
-
-      <FormSidePanel show={showForm} onClose={() => setShowForm(false)} formName="Artwork Inquiry Form">
-        <Field.Text name="firstName" placeholder="Answer" label="What's your first name?" required />
-        <Field.Text name="lastName" placeholder="Answer" label="What's your last name?" required />
-        <Field.Email name="email" placeholder="Answer" label="What's your email address?" required />
-        <Field.Text name="message" placeholder="Answer" label="Would you like to leave any message?" />
-      </FormSidePanel>
     </>
   );
 };
