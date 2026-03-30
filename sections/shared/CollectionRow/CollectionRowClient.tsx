c1f8244dbff785a11aa6386e8f6f1d0bed24538c
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2026-03-30 19:51:36 +0500

    changes

--- sections/shared/CollectionRow/CollectionRowClient.tsx
+++ sections/shared/CollectionRow/CollectionRowClient.tsx
@@ -360,7 +360,7 @@ const CollectionRowClient: React.FC<ICollectionRowClientProps> = ({ title, produ
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M15 18L9 12L15 6"
-                  stroke="currentColor"
+                  stroke="#333"
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -434,7 +434,7 @@ const CollectionRowClient: React.FC<ICollectionRowClientProps> = ({ title, produ
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M9 18L15 12L9 6"
-                  stroke="currentColor"
+                  stroke="#333"
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
